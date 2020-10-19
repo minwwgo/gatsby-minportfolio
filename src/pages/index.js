a1dc485f2e1d78d0ad6595dcac40c9aab522a5ee
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-10-19 02:08:41 +0100

    some update

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const Home=()=> {
     <Layout>
       <SEO title="Welcome to Min Blog" />
       <Container>
-        <h1>welcome from staging hello </h1>
+        <h1>welcome  staging hello </h1>
         <p>
           Go back <Link to={"/about"}>About</Link>
         </p>
